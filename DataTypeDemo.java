c54fb43ee05d2879f8242166a12f4f193d783576
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-19 15:32:56 +0530

    This is DataType program

---
+++ DataTypeDemo.java
@@ -0,0 +1,16 @@
+public class DataTypeDemo {
+    public static void main(String[] args){
+        int age = 21;
+        float height = 5.6f;
+        double weight = 58.75;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Deepika";
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Height: "+height);
+        System.out.println("Weight: "+weight);
+        System.out.println("Grade: "+grade);
+        System.out.println("Is Student: "+isStudent);
+    }
+}
