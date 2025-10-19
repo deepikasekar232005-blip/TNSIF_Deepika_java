a1436890ba3ea2a7af3d29ca48bfceeacfdbba1e
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-19 15:38:36 +0530

    This is If Else Program

---
+++ IfElseDemo.java
@@ -0,0 +1,11 @@
+public class IfElseDemo{
+    public static void main(String[] args){
+        int marks = 45;
+        if (marks >= 50){
+            System.out.println("You passed the exam!");
+        }
+        else{
+            System.out.println("You failed the exam.");
+        }
+    }
+}
