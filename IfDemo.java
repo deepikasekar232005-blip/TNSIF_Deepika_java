13e87a33a8e21e6016cb460c803a9e45ff5b5496
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-19 15:37:28 +0530

    This is If program

---
+++ IfDemo.java
@@ -0,0 +1,8 @@
+public class IfDemo {
+    public static void main(String[] args){
+        int marks = 85;
+        if (marks > 50){
+            System.out.println("You passed the exam!");
+        }
+    }
+}
