2da1b1dd977c552702f41c8e84bd6753cf041054
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-19 15:30:38 +0530

    This is a exmple of Continue Statement

---
+++ ContinueDemo.java
@@ -0,0 +1,11 @@
+public class ContinueDemo {
+    public static void main(String[] args){
+        for (int i = 1; i <= 5; i++){
+            if (i == 3) {
+                 System.out.println("skipping i = "+ i);
+                continue;
+            }
+            System.out.println("i = "+ i);
+        }
+    }
+}
