bc2c895989ceb5988b9c3f75a7f242d83571b11e
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-25 14:10:49 +0530

    This file contain finally block program

---
+++ FinallyBlockDemo.java
@@ -0,0 +1,13 @@
+public class FinallyBlockDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } 
+        catch (ArithmeticException e){
+            System.out.println("Exception caught:"+ e.getMessage());
+        }
+        finally {
+            System.out.println("Finally block always executes.");
+        }
+    }
+}
